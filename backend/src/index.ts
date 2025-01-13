9cd7d1f395b58ff608ca9ee34bbff6d80c03820f
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-13 12:35:39 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log("waheguru");
+// console.log("waheguru");
+
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {});
+});
