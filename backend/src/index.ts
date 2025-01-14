4e6955e20e3e47057ca795489dfa90ca8363a282
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-14 11:22:55 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,10 +7,26 @@ interface User {
   room: string;
 }
 
+let allSockets: User[] = [];
+
 const ws = new WebSocketServer({ port: 8080 });
 
 ws.on("connection", (socket) => {
   socket.on("message", (message) => {
     const parsedMessage = JSON.parse(message.toString());
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.room,
+      });
+    }
+    if (parsedMessage.type === "chat") {
+      let currentUserRoom = null;
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].socket === socket) {
+          currentUserRoom = allSockets[i].room;
+        }
+      }
+    }
   });
 });
