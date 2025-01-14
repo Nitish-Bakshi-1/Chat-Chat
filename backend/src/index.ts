22cd282f3740a1e45334c4947caf2032c3bf5044
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-14 11:03:47 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,15 @@
 
 import { WebSocket, WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
-
 interface User {
   socket: WebSocket;
   room: string;
 }
-interface joinMessage {
-  type: string;
-  payload: {
-    room: string;
-  };
-}
-wss.on("connection", (socket) => {
-  socket.on("message", (message) => {});
+
+const ws = new WebSocketServer({ port: 8080 });
+
+ws.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message.toString());
+  });
 });
