d9b1d291373f677bcf86a27643db668aa098a849
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-15 20:11:47 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,43 +7,36 @@ interface User {
   room: string;
   username: string;
 }
-interface Message {
-  text: string;
-  sender: string;
-}
 
 let allSockets: User[] = [];
 
 const ws = new WebSocketServer({ port: 8080 });
 
 ws.on("connection", (socket) => {
-  socket.on("message", (message: Message) => {
-    const parsedMessage = JSON.parse(message.toString());
+  socket.on("message", (message: string) => {
+    const parsedMessage = JSON.parse(message);
     const { username, room } = parsedMessage.payload;
     if (parsedMessage.type === "join") {
-      {
-        /*so here the message has to be of format    
-        message ={
-          type:"join",
-            payload:{
-            room :"red"
-          }}*/
+      if (username & room) {
+        allSockets.push({
+          socket,
+          room,
+          username,
+        });
       }
-      allSockets.push({
-        socket,
-        room,
-        username,
-      });
     }
     if (parsedMessage.type === "chat") {
       let currentUserRoom = null;
-      let currentUser = null;
+      let currentUser: User | null = null;
+
       for (let i = 0; i < allSockets.length; i++) {
         if (allSockets[i].socket === socket) {
           currentUserRoom = allSockets[i].room;
           currentUser = allSockets[i];
+          break;
         }
       }
+
       allSockets.forEach((s) => {
         if (s.room === currentUserRoom) {
           s.socket.send(
